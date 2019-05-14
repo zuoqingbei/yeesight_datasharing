eccab2ea6390586dfebc41eecaf1b2e5f08a56b5
Administrator <Administrator@DESKTOP-HNBDUU0>
2019-05-14 11:29:46 +0800

    ..

--- datamart-search/src/main/java/com/haier/datamart/interceptor/ParentInterceptors.java
+++ datamart-search/src/main/java/com/haier/datamart/interceptor/ParentInterceptors.java
@@ -29,7 +29,7 @@ public class ParentInterceptors extends WebMvcConfigurerAdapter {
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(
 				loginCeptor()
-				).excludePathPatterns("/user/login","/user/logout");
+				).excludePathPatterns("/user/login","/user/logout","/user/getUserByToken");
 	}
 	
  }
